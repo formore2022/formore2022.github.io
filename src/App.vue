3d9279dbc0048d99cc62145ac5bca596aca1839b
linziyou0601 <qqq23939889@gmail.com>
2022-03-29 00:41:30 +0800

    ADD 文章頁面及部分css

--- src/App.vue
+++ src/App.vue
@@ -1,82 +1,122 @@
 <template>
-  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <!-- 上方導航列 -->
+    <nav class="navbar navbar-expand-lg navbar-dark bg-main py-2 px-3">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">Navbar</a>
+            <button
+                class="navbar-toggler"
+                type="button"
+                data-bs-toggle="collapse"
+                data-bs-target="#navbarSupportedContent"
+                aria-controls="navbarSupportedContent"
+                aria-expanded="false"
+                aria-label="Toggle navigation"
+            >
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div
+                class="collapse navbar-collapse justify-content-between"
+                id="navbarSupportedContent"
+            >
+                <form class="d-flex">
+                    <input
+                        class="form-control me-2"
+                        type="search"
+                        placeholder="Search"
+                        aria-label="Search"
+                    />
+                </form>
+                <button class="btn btn-outline-light" type="submit">Search</button>
+            </div>
+        </div>
+    </nav>
+
+    <!-- 主內容區 -->
     <div class="container-fluid">
-      <a class="navbar-brand" href="#">Navbar</a>
-      <button
-        class="navbar-toggler"
-        type="button"
-        data-bs-toggle="collapse"
-        data-bs-target="#navbarSupportedContent"
-        aria-controls="navbarSupportedContent"
-        aria-expanded="false"
-        aria-label="Toggle navigation"
-      >
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div
-        class="collapse navbar-collapse justify-content-between"
-        id="navbarSupportedContent"
-      >
-        <form class="d-flex">
-          <input
-            class="form-control me-2"
-            type="search"
-            placeholder="Search"
-            aria-label="Search"
-          />
-        </form>
-        <button class="btn btn-outline-success" type="submit">Search</button>
-      </div>
+        <div class="row flex-nowrap">
+            <!-- 左側邊欄 -->
+            <div class="d-none d-sm-block col-sm-auto sidebar text-white min-vh-100 bg-gray py-3">
+                <ul class="nav flex-column align-items-start">
+                    <li class="nav-item">
+                        <router-link to="/hot" class="nav-link active">首頁</router-link>
+                    </li>
+                    <li class="nav-item">
+                        <router-link to="/About" class="nav-link">About</router-link>
+                    </li>
+                </ul>
+                <hr/>
+                <ul class="nav flex-column align-items-start">
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Link</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Link</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link disabled">Disabled</a>
+                    </li>
+                </ul>
+            </div>
+
+            <!-- 右側內容 -->
+            <div class="col">
+                <router-view />
+            </div>
+        </div>
     </div>
-  </nav>
-  <div class="container-fluid">
-    <div class="row">
-      <div class="col-auto">
-        <ul class="nav flex-column">
-          <li class="nav-item">
-            <router-link to="/" class="nav-link active">Home</router-link>
-          </li>
-          <li class="nav-item">
-            <router-link to="/About" class="nav-link">About</router-link>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">Link</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">Link</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link disabled">Disabled</a>
-          </li>
-        </ul>
-      </div>
-      <div class="col">
-        <router-view />
-      </div>
-    </div>
-  </div>
   
 </template>
 
 <style lang="scss">
+@import url('https://fonts.googleapis.com/css2?family=Noto+Sans+TC:wght@100;300;400;500;700;900&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap');
+
+:root {
+    --main-grean-color: #3C8585;
+    --dark-black-color: #303030;
+    --gray-color: #ECECEC;
+    --light-gray-color: #C9CACA;
+    --avatar-green-color: #235252;
+    --content-text-color: #7A7A7A;
+}
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+    font-family: Roboto, 'Noto Sans TC', Avenir, Helvetica, Arial, sans-serif;
+    font-weight: 400;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: var(--dark-black-color);
 }
 
 nav {
-  padding: 30px;
+    padding: 30px;
+}
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
+.bg-main {
+    background: var(--main-grean-color);
+}
 
-    &.router-link-exact-active {
-      color: #42b983;
+.bg-gray {
+    background: var(--gray-color);
+}
+
+.sidebar {
+    width: 200px !important;
+    font-size: 20px;
+
+    a {
+        padding: 0.25rem 1rem;
+        font-weight: bold;
+        color: var(--dark-black-color);
+
+        &.router-link-exact-active {
+            color: var(--main-grean-color);
+        }
     }
-  }
+
+    hr {
+        border-top: 2px solid var(--dark-black-color);
+        opacity: 1;
+    }
 }
 </style>
--- src/views/HotView.vue
+++ src/views/HotView.vue
@@ -1,5 +1,27 @@
 <template>
-  <div class="hot">
-    <h1>HOT</h1>
-  </div>
+    <div class="hot">
+        <div class="row row-cols-1 row-cols-md-2 row-cols-xxl-3 g-5">
+            <div class="col" v-for="(post, index) in posts" :key="index">
+                <PostCard :post="post"/>
+            </div>
+        </div>
+    </div>
 </template>
+
+<script>
+// 引入component "PostCard" 進行使用
+import PostCard from '../components/PostCard.vue'
+
+export default {
+    components: {
+        PostCard,
+    }
+};
+</script>
+
+<script setup>
+import { inject } from 'vue';
+
+// inject需要用的參數進行使用，需在parent或grand-parent進行provide
+const posts = inject('hotPosts')
+</script>
