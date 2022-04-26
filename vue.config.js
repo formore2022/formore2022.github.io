15307c9ef73aeec93e4fdf3a31257f49bbffd724
linziyou0601 <qqq23939889@gmail.com>
2022-04-26 09:05:44 +0800

    FIX ERROR

--- vue.config.js
+++ vue.config.js
@@ -3,5 +3,10 @@ module.exports = defineConfig({
   transpileDependencies: true,
   publicPath: process.env.NODE_ENV === 'production'
     ? '/'
-    : '/'
+    : '/',
+  chainWebpack: (config) => {
+    config.module.rule('images')
+      .test(/\.(png|jpe?g|gif|webp|avif|svg)(\?.*)?$/i)
+      .set('type', 'asset')
+  },
 })
