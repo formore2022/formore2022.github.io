2984a6d56c08349f0960c2fac744947a17ba692a
linziyou0601 <qqq23939889@gmail.com>
2022-04-26 09:10:08 +0800

    FIX 強制修正檔案副檔名不能為大寫

--- vue.config.js
+++ vue.config.js
@@ -3,5 +3,10 @@ module.exports = defineConfig({
   transpileDependencies: true,
   publicPath: process.env.NODE_ENV === 'production'
     ? '/formore'
-    : '/'
+    : '/',
+  chainWebpack: (config) => {
+    config.module.rule('images')
+      .test(/\.(png|jpe?g|gif|webp|avif|svg)(\?.*)?$/i)
+      .set('type', 'asset')
+  },
 })
